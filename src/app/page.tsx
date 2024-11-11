b29ca332b0f77470603c6d35b0cb1cf1f356e361
Kinza Aftab <160029911+KinzaAft@users.noreply.github.com>
2024-11-11 11:13:54 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,11 +49,11 @@ export default function Home() {
   </a>
 </div>
 
-<div className="third-section h-[800px] md:h-[400px] bg-slate-100 mt-12 px-4 md:px-16">
+<div className="third-section h-[800px] md:h-[600px] bg-slate-100 mt-12 px-4 md:px-16">
   <h1 className="text-4xl text-[#10375C] text-center font-extrabold pt-10">SKILLS & SERVICES</h1>
   <div className="flex flex-wrap justify-center mt-8">
     {skillData.map((IData, index) => (
-      <div key={index} className="shadow h-[200px] w-full md:w-[600px] border-2 border-orange-600 rounded-2xl m-4 p-4">
+      <div key={index} className="shadow h-[200px] w-full md:w-[400px] border-2 border-orange-600 rounded-2xl m-4 p-4">
         <p className="text-center text-[#10375C] font-medium text-2xl mt-[30px]">{IData.tittle}</p>
         <p className="text-center mt-[20px] text-lg text-[#10375C]">{IData.description}</p>
       </div>
